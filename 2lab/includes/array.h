1c443cec38894bb6597ccf84b3064799dd3264a5
ownage69 <maks.samoylov2007@gmail.com>
2025-09-16 18:30:37 +0300

    Fix SonarCloud issues

--- 2lab/includes/array.h
+++ 2lab/includes/array.h
@@ -20,7 +20,35 @@ public:
     const int& operator[](size_t index) const;
     size_t getSize() const { return size; }
 
-    friend Array operator+(const Array& lhs, const Array& rhs);
-    friend std::ostream& operator<<(std::ostream& os, const Array& arr);
-    friend std::istream& operator>>(std::istream& is, Array& arr);
+    friend Array operator+(const Array& lhs, const Array& rhs) {
+        Array result(lhs.size + rhs.size);
+        std::copy(lhs.data, lhs.data + lhs.size, result.data);
+        std::copy(rhs.data, rhs.data + rhs.size, result.data + lhs.size);
+        return result;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Array& arr) {
+        os << arr.size << ": ";
+        for (size_t i = 0; i < arr.size; ++i) {
+            os << arr.data[i];
+            if (i < arr.size - 1) os << " ";
+        }
+        os << std::endl;
+        return os;
+    }
+
+    friend std::istream& operator>>(std::istream& is, Array& arr) {
+        size_t newSize;
+        is >> newSize;
+        if (is.fail()) return is;
+
+        delete[] arr.data;
+        arr.size = newSize;
+        arr.data = newSize ? new int[newSize] : nullptr;
+
+        for (size_t i = 0; i < newSize; ++i) {
+            is >> arr.data[i];
+        }
+        return is;
+    }
 };
--- 2lab/src/array.cpp
+++ 2lab/src/array.cpp
@@ -25,42 +25,10 @@ Array& Array::operator=(const Array& other) {
     return *this;
 }
 
-Array operator+(const Array& lhs, const Array& rhs) {
-    Array result(lhs.size + rhs.size);
-    std::copy(lhs.data, lhs.data + lhs.size, result.data);
-    std::copy(rhs.data, rhs.data + rhs.size, result.data + lhs.size);
-    return result;
-}
-
 int& Array::operator[](size_t index) {
     return data[index];
 }
 
 const int& Array::operator[](size_t index) const {
     return data[index];
-}
-
-std::ostream& operator<<(std::ostream& os, const Array& arr) {
-    os << arr.size << ": ";
-    for (size_t i = 0; i < arr.size; ++i) {
-        os << arr.data[i];
-        if (i < arr.size - 1) os << " ";
-    }
-    os << std::endl;
-    return os;
-}
-
-std::istream& operator>>(std::istream& is, Array& arr) {
-    size_t newSize;
-    is >> newSize;
-    if (is.fail()) return is;
-
-    delete[] arr.data;
-    arr.size = newSize;
-    arr.data = newSize ? new int[newSize] : nullptr;
-
-    for (size_t i = 0; i < newSize; ++i) {
-        is >> arr.data[i];
-    }
-    return is;
 }
