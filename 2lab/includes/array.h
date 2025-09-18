5935b3e681a6ae53e8c677db0d9591380237f846
ownage69 <maks.samoylov2007@gmail.com>
2025-09-18 18:39:27 +0300

    Update whole code

--- 2lab/includes/array.h
+++ 2lab/includes/array.h
@@ -1,30 +1,23 @@
 #pragma once
-
-#include <cstddef>
 #include <iostream>
 
 class Array {
 private:
-    size_t size;
+    int size;
     int* data;
 
 public:
-    explicit Array(size_t s = 0);
-    ~Array();
+    Array();
+    explicit Array(int n);
     Array(const Array& other);
     Array& operator=(const Array& other);
+    ~Array();
 
-    int& operator[](size_t index) {
-        return data[index];
-    }
+    int getSize() const;
+    int& operator[](int index);
+    const int& operator[](int index) const;
 
-    const int& operator[](size_t index) const {
-        return data[index];
-    }
+    Array operator!() const;
 
-    size_t getSize() const { return size; }
-
-    friend Array operator+(const Array& lhs, const Array& rhs);
     friend std::ostream& operator<<(std::ostream& os, const Array& arr);
-    friend std::istream& operator>>(std::istream& is, Array& arr);
 };
--- 2lab/src/array.cpp
+++ 2lab/src/array.cpp
@@ -1,59 +1,84 @@
 #include "array.h"
-#include <algorithm>
+#include <stdexcept>
 
-Array::Array(size_t s) : size(s), data(s ? new int[s] : nullptr) {
+Array::Array() {
+    size = 0;
+    data = nullptr;
 }
 
-Array::~Array() {
-    delete[] data;
+Array::Array(int n) {
+    if (n < 0) throw std::invalid_argument("Size cannot be negative");
+    if (n == 0) {
+        size = 0;
+        data = nullptr;
+        return;
+    }
+    size = n;
+    data = new int[size];
+    for (int i = 0; i < size; ++i) data[i] = 0;
 }
 
-Array::Array(const Array& other) : size(other.size), data(other.size ? new int[other.size] : nullptr) {
-    if (data) {
-        std::copy(other.data, other.data + size, data);
+Array::Array(const Array& other) {
+    if (other.size > 0) {
+        size = other.size;
+        data = new int[size];
+        for (int i = 0; i < size; ++i) data[i] = other.data[i];
+    }
+    else {
+        size = 0;
+        data = nullptr;
     }
 }
 
 Array& Array::operator=(const Array& other) {
-    if (this != &other) {
-        delete[] data;
-        size = other.size;
-        data = other.size ? new int[other.size] : nullptr;
-        if (data) {
-            std::copy(other.data, other.data + size, data);
-        }
+    if (this == &other) return *this;
+    int* new_data = nullptr;
+    if (other.size > 0) {
+        new_data = new int[other.size];
+        for (int i = 0; i < other.size; ++i) new_data[i] = other.data[i];
     }
+    delete[] data;
+    data = new_data;
+    size = other.size;
     return *this;
 }
 
-Array operator+(const Array& lhs, const Array& rhs) {
-    Array result(lhs.size + rhs.size);
-    std::copy(lhs.data, lhs.data + lhs.size, result.data);
-    std::copy(rhs.data, rhs.data + rhs.size, result.data + lhs.size);
+Array::~Array() {
+    delete[] data;
+    data = nullptr;
+    size = 0;
+}
+
+int Array::getSize() const {
+    return size;
+}
+
+int& Array::operator[](int index) {
+    if (index < 0 || index >= size) throw std::out_of_range("Index out of range");
+    return data[index];
+}
+
+const int& Array::operator[](int index) const {
+    if (index < 0 || index >= size) throw std::out_of_range("Index out of range");
+    return data[index];
+}
+
+Array Array::operator!() const {
+    if (size == 0) return Array();
+    Array result(size * 2);
+    for (int i = 0; i < size; ++i) {
+        result.data[i] = data[i];
+        result.data[i + size] = data[i];
+    }
     return result;
 }
 
 std::ostream& operator<<(std::ostream& os, const Array& arr) {
-    os << arr.size << ": ";
-    for (size_t i = 0; i < arr.size; ++i) {
+    os << arr.size << ":";
+    if (arr.size > 0) os << ' ';
+    for (int i = 0; i < arr.size; ++i) {
         os << arr.data[i];
-        if (i < arr.size - 1) os << " ";
+        if (i + 1 < arr.size) os << ' ';
     }
-    os << std::endl;
     return os;
-}
-
-std::istream& operator>>(std::istream& is, Array& arr) {
-    size_t newSize;
-    is >> newSize;
-    if (is.fail()) return is;
-
-    delete[] arr.data;
-    arr.size = newSize;
-    arr.data = newSize ? new int[newSize] : nullptr;
-
-    for (size_t i = 0; i < newSize; ++i) {
-        is >> arr.data[i];
-    }
-    return is;
 }
