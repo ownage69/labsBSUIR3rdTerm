199d8276d8473fc9483017848fa95a20a84100ba
ownage69 <maks.samoylov2007@gmail.com>
2025-09-19 21:54:08 +0300

    Fix SonarCloud issues

--- 2lab/src/array.cpp
+++ 2lab/src/array.cpp
@@ -6,19 +6,16 @@ Array::Array() : size(0), data(nullptr) {}
 
 Array::Array(int n) {
     if (n <= 0) {
-        size = 0;
-        data = nullptr;
+        throw std::invalid_argument("Array size must be positive");
     }
-    else {
-        size = n;
-        data = new int[size];
-        std::fill(data, data + size, 0);
-    }
+    size = n;
+    data = new int[size];
+    std::fill(data, data + size, 0);
 }
 
 Array::Array(const Array& other) : size(other.size), data(nullptr) {
     if (size > 0) {
-        data = new int[size]();
+        data = new int[size];
         for (int i = 0; i < size; ++i) data[i] = other.data[i];
     }
 }
@@ -32,10 +29,12 @@ Array& Array::operator=(const Array& other) {
     if (this == &other) return *this;
     delete[] data;
     size = other.size;
-    data = nullptr;
     if (size > 0) {
-        data = new int[size]();
+        data = new int[size];
         for (int i = 0; i < size; ++i) data[i] = other.data[i];
+    }
+    else {
+        data = nullptr;
     }
     return *this;
 }
@@ -59,11 +58,17 @@ int Array::getSize() const {
 }
 
 int& Array::operator[](int index) {
+    if (size == 0 || data == nullptr) {
+        throw std::out_of_range("Access to empty array");
+    }
     if (index < 0 || index >= size) throw std::out_of_range("Index out of range");
     return data[index];
 }
 
 const int& Array::operator[](int index) const {
+    if (size == 0 || data == nullptr) {
+        throw std::out_of_range("Access to empty array");
+    }
     if (index < 0 || index >= size) throw std::out_of_range("Index out of range");
     return data[index];
 }
