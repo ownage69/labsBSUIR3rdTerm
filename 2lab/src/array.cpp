86aa0eaeb00bc7d18d14471b560f890c8a356eb9
ownage69 <maks.samoylov2007@gmail.com>
2025-09-22 17:18:36 +0300

    Fix SonarCloud issues

--- 2lab/src/array.cpp
+++ 2lab/src/array.cpp
@@ -82,29 +82,3 @@ Array Array::operator!() const {
     }
     return result;
 }
-
-std::ostream& operator<<(std::ostream& os, const Array& arr) {
-    os << arr.size << ":";
-    if (arr.size > 0) os << ' ';
-    for (int i = 0; i < arr.size; ++i) {
-        os << arr.data[i];
-        if (i + 1 < arr.size) os << ' ';
-    }
-    return os;
-}
-
-std::istream& operator>>(std::istream& is, Array& arr) {
-    delete[] arr.data;
-    is >> arr.size;
-    if (arr.size <= 0) {
-        arr.size = 0;
-        arr.data = nullptr;
-    }
-    else {
-        arr.data = new int[arr.size];
-        for (int i = 0; i < arr.size; ++i) {
-            is >> arr.data[i];
-        }
-    }
-    return is;
-}
