8e128fce4c139ffa390e29ee35a62ad756394cf8
ownage69 <maks.samoylov2007@gmail.com>
2025-09-27 16:47:31 +0300

    Update File

--- 3lab/src/SolvableQuadraticEquation.cpp
+++ 3lab/src/SolvableQuadraticEquation.cpp
@@ -1,7 +1,45 @@
 ﻿#include "SolvableQuadraticEquation.h"
 
 SolvableQuadraticEquation::SolvableQuadraticEquation(int a_val, int b_val, int c_val)
-    : QuadraticEquation(a_val, b_val), c(c_val), x1(0.0), x2(0.0), hasRealRoots(false) {
+    : QuadraticEquation(a_val, b_val), c(c_val), roots(nullptr), numRoots(0) {
+}
+
+SolvableQuadraticEquation::SolvableQuadraticEquation(const SolvableQuadraticEquation& other)
+    : QuadraticEquation(other), c(other.c), numRoots(other.numRoots) {
+    if (numRoots > 0) {
+        roots = new double[numRoots];
+        for (int i = 0; i < numRoots; ++i) {
+            roots[i] = other.roots[i];
+        }
+    }
+    else {
+        roots = nullptr;
+    }
+}
+
+SolvableQuadraticEquation& SolvableQuadraticEquation::operator=(const SolvableQuadraticEquation& other) {
+    if (this == &other) return *this;
+
+    QuadraticEquation::operator=(other);
+    c = other.c;
+    numRoots = other.numRoots;
+
+    delete[] roots;
+    if (numRoots > 0) {
+        roots = new double[numRoots];
+        for (int i = 0; i < numRoots; ++i) {
+            roots[i] = other.roots[i];
+        }
+    }
+    else {
+        roots = nullptr;
+    }
+
+    return *this;
+}
+
+SolvableQuadraticEquation::~SolvableQuadraticEquation() {
+    delete[] roots;
 }
 
 int SolvableQuadraticEquation::getC() const {
@@ -14,7 +52,9 @@ void SolvableQuadraticEquation::setC(int c_val) {
 
 void SolvableQuadraticEquation::solve() {
     if (a == 0) {
-        hasRealRoots = false;
+        delete[] roots;
+        roots = nullptr;
+        numRoots = 0;
         return;
     }
 
@@ -24,21 +64,29 @@ void SolvableQuadraticEquation::solve() {
 
     double discriminant = db * db - 4 * da * dc;
 
+    delete[] roots;
+    roots = nullptr;
+    numRoots = 0;
+
     if (discriminant > 0.0) {
+        numRoots = 2;
+        roots = new double[2];
         double sqrtDisc = std::sqrt(discriminant);
-        x1 = (-db - sqrtDisc) / (2 * da);
-        x2 = (-db + sqrtDisc) / (2 * da);
-        if (x1 > x2) {
-            std::swap(x1, x2);
+        roots[0] = (-db - sqrtDisc) / (2 * da);
+        roots[1] = (-db + sqrtDisc) / (2 * da);
+        if (roots[0] > roots[1]) {
+            std::swap(roots[0], roots[1]);
         }
-        hasRealRoots = true;
     }
     else if (discriminant == 0.0) {
-        x1 = x2 = -db / (2 * da);
-        hasRealRoots = true;
+        numRoots = 2; 
+        roots = new double[2];
+        double root = -db / (2 * da);
+        roots[0] = root;
+        roots[1] = root;
     }
     else {
-        hasRealRoots = false;
+        numRoots = 0;
     }
 }
 
@@ -48,9 +96,11 @@ void SolvableQuadraticEquation::printRoots() const {
         return;
     }
 
-    if (hasRealRoots) {
-        std::cout << "Первый корень x1: " << x1 << std::endl;
-        std::cout << "Второй корень x2: " << x2 << std::endl;
+    if (numRoots > 0) {
+        std::cout << "Первый корень x1: " << roots[0] << std::endl;
+        if (numRoots == 2) {
+            std::cout << "Второй корень x2: " << roots[1] << std::endl;
+        }
     }
     else {
         std::cout << "Дискриминант отрицательный: нет реальных корней." << std::endl;
