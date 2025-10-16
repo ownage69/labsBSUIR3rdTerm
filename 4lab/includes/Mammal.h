4cbf9e23231d08b4ebaa7b9c441d94125aff21c5
ownage69 <changehwid518@gmail.com>
2025-10-16 12:36:00 +0300

    Update files

--- 4lab/includes/Mammal.h
+++ 4lab/includes/Mammal.h
@@ -3,11 +3,11 @@
 
 class Mammal {
 private:
-    std::string name;
+	std::string name;
 public:
-    explicit Mammal(const std::string& name = "Unnamed") : name(name) {}
-    virtual ~Mammal() = default;
+	explicit Mammal(const std::string& name = "Unnamed") : name(name) {}
+	virtual ~Mammal() = default;
 
-    virtual void describe() const = 0;
-    std::string getName() const { return name; }
+	virtual void describe() const = 0;
+	std::string getName() const { return name; }
 };
