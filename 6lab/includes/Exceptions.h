f1512692e6ce61adb292b2aa7d43014eb318eb11
ownage69 <changehwid518@gmail.com>
2025-10-25 11:56:09 +0300

    Update file

--- 6lab/includes/Exceptions.h
+++ 6lab/includes/Exceptions.h
@@ -2,14 +2,13 @@
 #include <exception>
 #include <string>
 
-using namespace std;
-
-class InvalidCharacterException : public exception {
-    string errormessage;
+class InvalidCharacterException : public std::exception {
+    std::string errormessage;
 public:
-    InvalidCharacterException(const string& input, const string& invalidChars) {
+    InvalidCharacterException(const std::string& input, const std::string& invalidChars) {
         errormessage = "Ошибка ввода: найдены недопустимые символы: '" + invalidChars + "' в строке: \"" + input + "\"";
     }
+
     const char* what() const noexcept override {
         return errormessage.c_str();
     }
