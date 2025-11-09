8dcf2dc6286036d8e119a759be83600fe68c0ebe
ownage69 <changehwid518@gmail.com>
2025-11-09 11:49:16 +0300

    Update class

--- 6lab/includes/EmptyInputException.h
+++ 6lab/includes/EmptyInputException.h
@@ -1,14 +1,14 @@
 #pragma once
+
 #include <exception>
 #include <string>
+#include <string_view>
 
 class EmptyInputException : public std::exception {
     std::string message;
 
 public:
-    explicit EmptyInputException(const std::string& input) {
-        message = "Ошибка ввода: строка пустая";
-    }
+    explicit EmptyInputException(std::string_view) : message("Ошибка ввода: строка пустая") {}
     const char* what() const noexcept override {
         return message.c_str();
     }
