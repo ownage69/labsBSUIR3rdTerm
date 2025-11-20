13ab38ecb28e1c176f8adb677b30752536673b27
Максим <changehwid518@gmail.com>
2025-11-20 20:18:44 +0300

    Delete 8lab directory

--- 8lab/includes/safeInput.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <string>
-
-int safeInputInt(const std::string &prompt);
-int safePositiveInputInt(const std::string &prompt);
-std::string readLineTrimmed(const std::string &prompt);
-
--- 8lab/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "menu.h"
-
-int main()
-{
-    Menu menu;
-    menu.run();
-    return 0;
-}
